487112479cb9d272d096b72597806256675da095
Palacio <guipalacio9@hotmail.com>
2022-11-07 17:59:01 -0400

    feat: Slides + Slide Thumbs Navigation

--- gulpfile.js
+++ gulpfile.js
@@ -15,15 +15,16 @@ const compilaSass = () => {
         overrideBrowserslist: ['last 2 versions'] , 
         cascade: false 
     }))
-    .pipe(gulp.dest('src/css'))
+    .pipe(gulp.dest('src/css/'))
     .pipe(browserSync.stream())
 }
 gulp.task('sass', compilaSass);
 
 const libsCss = () => {
-    return gulp.src('./src/css/lib/*.css')
+    return gulp
+    .src('./src/css/lib/*.css')
     .pipe(concat('libs.css'))
-    .pipe(gulp.dest('./src/css'))
+    .pipe(gulp.dest('./src/css/'))
     .pipe(browserSync.stream());
 }
 gulp.task('libsCss', libsCss);
@@ -35,30 +36,20 @@ const gulpJs = () => {
         presets: ['@babel/env']
     }))
     .pipe(uglify())
-    .pipe(gulp.dest('src/js'))
-    .pipe(browserSync.stream())
+    .pipe(gulp.dest('src/js/'))
+    .pipe(browserSync.stream());
 }
-gulp.task('appJs', gulpJs)
+gulp.task('appJs', gulpJs);
 
 // Plugins / bibliotecas
 const libsJs = () => {
     return gulp
     .src(['./src/js/lib/aos.min.js', './src/js/lib/swiper.min.js'])
     .pipe(concat('libs.js'))
-    .pipe(gulp.dest('src/js'))
+    .pipe(gulp.dest('src/js/'))
     .pipe(browserSync.stream());
 }
 gulp.task('libsJs', libsJs);
-
-// Watcher para alterações em sass e html 
-const watcher = () => {
-    gulp.watch('src/scss/*.scss', compilaSass);
-    gulp.watch('*.html').on('change', browserSync.reload);
-    gulp.watch('src/js/scripts/*.js', gulpJs);
-    gulp.watch('src/js/libs/*.js', libsJs);
-    // gulp.watch('src/css/*.css', libsCss);
-}
-gulp.task('watch', watcher);
 
 // Cria o nosso browser a partir do html
 const browser = () => {
@@ -69,6 +60,16 @@ const browser = () => {
     });
 }
 gulp.task('browser-sync', browser)
+
+// Watcher para alterações em sass e html 
+const watcher = () => {
+    gulp.watch('src/scss/*.scss', compilaSass);
+    gulp.watch('*.html').on('change', browserSync.reload);
+    gulp.watch('src/js/scripts/*.js', gulpJs);
+    gulp.watch('src/js/libs/*.js', libsJs);
+    // gulp.watch('src/css/*.css', libsCss);
+}
+gulp.task('watch', watcher);
 
 // Executa as funçoes watcher e browserSync
 gulp.task('default', gulp.parallel(watcher, browser, compilaSass, libsCss, gulpJs, libsJs));  
--- src/js/scripts/main.js
+++ src/js/scripts/main.js
@@ -1,3 +1,19 @@
-var slide_hero = new Swiper(".slide-principal", {
-    effect: 'creative'
+const slide_thumbnail = new Swiper('.slide-thumbnail', {
+    slidesPerView: 5,
+    direction: 'vertical',
+    spaceBetween: 20,
+    watchSlideProgress: true
 });
+
+const slide_hero = new Swiper('.slide-principal', {
+    effect: 'fade',
+    thumbs: {
+        swiper: slide_thumbnail,
+    },
+    autoplay: {
+        delay: 5000,
+        disableOnInteraction: false,
+    }
+})
+
+
