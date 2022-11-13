6244c576763024faa324220e28d8a983c9bb35a8
Palacio <guipalacio9@hotmail.com>
2022-11-12 20:54:33 -0400

    feat: Modal funcionando

--- src/js/scripts/main.js
+++ src/js/scripts/main.js
@@ -36,5 +36,17 @@ allFilters.forEach((filter, index) => {
     })
 })
 
+const btnOpenModal = document.querySelector('.js-open-modal');
+const btnCloseModal = document.querySelector('.js-close');
 
+btnOpenModal.addEventListener('click', (event) => {
+    event.preventDefault();
+    let tagHtml = document.documentElement;
+    tagHtml.classList.add('show-modal')
+});
+
+btnCloseModal.addEventListener('click', () => {
+    let tagHtml = document.documentElement;
+    tagHtml.classList.remove('show-modal')
+});
 
